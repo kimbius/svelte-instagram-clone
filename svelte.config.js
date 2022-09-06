351d4062d964f70de4527099969e2dbebd99c3a6
Peerawit Piwsawang <peerawit@MacBook-Air-khxng-Peerawit.local>
2022-09-07 01:06:13 +0700

    svelte cloudflare static site deploy 🎃

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
 	}, preprocess: [
 		preprocess({
 			postcss: true,
 		}),
-	],
+	], prerender: {
+		default: true
+	}
 };
 
 export default config;
